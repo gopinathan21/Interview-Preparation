0f3378cfbe009e83c90c1c94326c706aee211a8d
Your Name <gopinathanselvaraj21@gmail.com>
2025-10-04 20:16:23 +0530

    added tests in main

--- DSA/Main.java
+++ DSA/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         sum.Test.runTests();
         check_subsequence_sum.Test.runTests();
+        count_subsequence_sum.Test.runTests();
+        combination_sum.Test.runTests();
+        power_set.Test.runTests();
     }
 }
